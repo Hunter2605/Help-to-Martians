a562d81826999a4aff8a1cd7cf7111803b797de5
Hantemir Menishov <menisovh@gmail.com>
2024-11-28 11:18:41 +0600

    changed main

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,6 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int firstGuess = sc.nextInt();
-        int secondGuess = sc.nextInt();
-        int thirdGuess = sc.nextInt();
         ArrayList<Integer> guesses = new ArrayList<>();
         ArrayList<Integer> locationOfBoxes = new ArrayList<>();
     }
