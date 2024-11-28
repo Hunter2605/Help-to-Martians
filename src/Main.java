04bddeb7f3203cd92255a4db722afbd88fda9654
Hantemir Menishov <menisovh@gmail.com>
2024-11-28 11:40:15 +0600

    added method call

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,24 @@ import java.util.ArrayList;
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         ArrayList<Integer> guesses = new ArrayList<>();
         ArrayList<Integer> locationOfBoxes = new ArrayList<>();
+        int attempts = 0;
+        int maxAttempts = 5;
+        RandomBoxes(locationOfBoxes);
+        while (attempts < maxAttempts) {
+            Guesses(guesses);
+            System.out.println(checkBoxes(locationOfBoxes, guesses));
+            guesses.clear();
+            attempts++;
+        }
     }
     static void Guesses(ArrayList<Integer> guesses) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Input 3 digits: ");
         for (int i = 0; i < 3; i++) {
             int guess = sc.nextInt();
-            while (guess>7 || guess<0 ) {
+            if (guess>7 || guess<0 ) {
                 System.out.println("Incorrect input. Try again, give an integer between 0 and 7.");
             }
             guesses.add(guess);
@@ -25,5 +34,14 @@ public class Main {
                 locationOfBoxes.add(location);
             }
         }
+    }
+    static int checkBoxes(ArrayList<Integer> locationOfBoxes, ArrayList<Integer> guesses) {
+        int correct = 0;
+        for (int i = 0; i < 3; i++) {
+            if (locationOfBoxes.contains(guesses.get(i))) {
+                correct++;
+            }
+        }
+        return correct;
     }
 }
