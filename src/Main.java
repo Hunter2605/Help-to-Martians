cb163490a4f1569020ff05dd98ffdf2df2554529
Hantemir Menishov <menisovh@gmail.com>
2024-11-28 11:13:01 +0600

    add new method

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,23 @@ public class Main {
         ArrayList<Integer> guesses = new ArrayList<>();
         ArrayList<Integer> locationOfBoxes = new ArrayList<>();
     }
-    public static void RandomBoxesLocation() {
-
+    static void Guesses(ArrayList<Integer> guesses) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            int guess = sc.nextInt();
+            while (guess>7 || guess<0 ) {
+                System.out.println("Incorrect input. Try again, give an integer between 0 and 7.");
+            }
+            guesses.add(guess);
+        }
+    }
+    static void RandomBoxes(ArrayList<Integer> locationOfBoxes) {
+        Random rand = new Random();
+        while (locationOfBoxes.size()<3) {
+            int location = rand.nextInt(8);
+            if (!locationOfBoxes.contains(location)) {
+                locationOfBoxes.add(location);
+            }
+        }
     }
 }
